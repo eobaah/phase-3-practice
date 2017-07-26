ae95cb1a825a78f702245d0a5a4af6c178340abb
eobaah <eobaah@gmail.com>
2017-07-26 11:52:00 -0700

    wip update totals in modal

--- part-3/script.js
+++ part-3/script.js
@@ -1,13 +1,17 @@
-let counter = 0
-
 let addBtn = document.querySelectorAll('.add-cart-btn')
 let cartBtn = document.getElementById('cart-button')
 let closeBtn = document.querySelector('.close-modal')
+let clearBtn = document.querySelector('.clear-cart')
+let cartCounter = document.getElementById('cart-item-count')
+let cart = document.getElementById( 'add-items-modal' )
+let cartTotal = document.getElementById( 'cart-item-count' )
+let modalTotal = document.querySelector('.modal-total')
 let cartArray = []
 let cartItemsArr = []
+let cartPriceArr = []
 
 function increase() {
-  document.getElementById('cart-item-count').innerHTML = cartArray.length+1
+  cartCounter.innerHTML = cartArray.length+1
 }
 
 function modal() {
@@ -17,11 +21,47 @@ function modal() {
   element.style.visibility = (element.style.visibility == 'visible') ? 'hidden' : 'visible';
 }
 
-function cartItems() {
-  cartItemsArr = Object.keys(cartArray)
-  for( let index = 0; index <= cartArray.length; index++ ){
-    alert( cartArray )
+function sumCartItems() {
+  cartTotal = cartPriceArr.reduce( (sum, item) => {
+    return sum + Number(item)
+  }, 0)
+  modalTotal.innerHTML = `$ ${cartTotal.toFixed(2)}`
+}
+
+function clearCart() {
+  emptyCartElements()
+  cartCounter.innerHTML = "(0)"
+  modalTotal.innerHTML = "$0.00"
+}
+
+
+function populateCart() {
+  cartArray.forEach( element => {
+    for ( var k in element ) {
+      cartItemsArr.push( k )
+      cartPriceArr.push( element[k] )
+    }
+  })
+  for( let index = 0; index < cartArray.length; index++) {
+    let itemPriceNode = document.createElement( 'DIV' );
+    let itemNameNode = document.createElement( 'SPAN' );
+    let priceNameNode = document.createElement( 'SPAN' );
+    let textNode = document.createTextNode( cartItemsArr[ index ] );
+    let priceNode = document.createTextNode( cartPriceArr[ index ] );
+    itemNameNode.appendChild( textNode );
+    priceNameNode.appendChild( priceNode );
+    itemPriceNode.appendChild( itemNameNode );
+    itemPriceNode.appendChild( priceNameNode );
+    cart.appendChild( itemPriceNode )
   }
+}
+
+function emptyCartElements() {
+  cartArray = []
+  while (cart.hasChildNodes()) {
+      cart.removeChild(cart.firstChild);
+  }
+  modalTotal.innerHTML = "$0.00"
 }
 
 addBtn.forEach( (target) => {
@@ -30,11 +70,14 @@ addBtn.forEach( (target) => {
   let item = JSON.parse(target.getAttribute('data-item'))
   cartArray.push( item )
   console.log( cartArray )
+  return cartArray
   });
 });
 
+
 cartBtn.addEventListener('click', () => {
-  cartItems()
+  populateCart()
+  sumCartItems()
   modal()
 });
 
@@ -42,11 +85,7 @@ closeBtn.addEventListener('click', (target) => {
    modal()
 });
 
-
-
-
-//
-// var node = document.createElement("DIV");                 // Create a <li> node
-// var textnode = document.createTextNode("Water");         // Create a text node
-// node.appendChild(textnode);                              // Append the text to <li>
-// document.getElementById("myList").appendChild(node);     // Append <li> to <ul> with id="myList"
+clearBtn.addEventListener('click', () => {
+   clearCart()
+   modal()
+});
